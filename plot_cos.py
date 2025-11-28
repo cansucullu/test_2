37f6e81c17d73fcf63880f914092765902f43595
Can Sücüllü <cansucullu@gmail.com>
2025-11-28 02:10:53 +0000

    Move common plotting functions to plot_utils file

--- plot_cos.py
+++ plot_cos.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from plot_utils import multiple_formatter
+
 in_array = np.linspace(-np.pi*3, np.pi*3, 120)
 out_array = np.cos(in_array)
 
@@ -8,8 +10,12 @@ print("in_array : ", in_array)
 print("\nout_array : ", out_array)
 
 # blue for numpy.cos()
-plt.plot(in_array, out_array, color = 'blue', marker = "o")
-plt.title("numpy.cos()")
-plt.xlabel("X")
-plt.ylabel("Y")
+fig, ax = plt.subplots()
+ax.plot(in_array, out_array, color = 'blue', marker = "o")
+ax.set_title("numpy.cos()")
+ax.set_xlabel("X")
+ax.set_xlabel("Y")
+ax.xaxis.set_major_locator(plt.MultipleLocator(np.pi / 2))
+ax.xaxis.set_minor_locator(plt.MultipleLocator(np.pi / 12))
+ax.xaxis.set_major_formatter(plt.FuncFormatter(multiple_formatter()))
 plt.show()
